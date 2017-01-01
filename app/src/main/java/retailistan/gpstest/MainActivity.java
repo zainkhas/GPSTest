536e0ad079bc2cb3845a2b6585210659e6df0f8f
Zain <zenkhas@gmail.com>
2017-01-01 19:42:36 +0500

    Changes :P

--- app/src/main/java/retailistan/gpstest/MainActivity.java
+++ app/src/main/java/retailistan/gpstest/MainActivity.java
@@ -3,6 +3,7 @@ package retailistan.gpstest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -20,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    private void makingChanges(){
+        Log.d(TAG,"This is the change I have made!");
     }
 }
