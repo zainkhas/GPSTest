10e815cc4c21dd235f40f5bd35f4d0d9fbb0a760
Zain <zenkhas@gmail.com>
2017-01-26 17:14:32 +0500

    new scenario

--- app/src/main/java/retailistan/gpstest/MainActivity.java
+++ app/src/main/java/retailistan/gpstest/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void showData(){
 
-        Toast.makeText(this, "Hello JS!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello Abrar!", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Hello Zain!", Toast.LENGTH_SHORT).show();
     }
 }
